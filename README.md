b77b396d6ca4b10d8179458c5d79c969391cc570
ekajogja <ekajogja@gmail.com>
2015-04-08 11:36:33 +0700

    complete rewrite and build v15.04

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-Official [RadioBuku](http://radiobuku.com) streaming app built using Intel XDK and Ratchet.
+Official [RadioBuku](http://radiobuku.com) streaming app built using Intel XDK + Bootstrap 3.
 
 Build:
 
 1. [v15.03 "Zeta" --> radiobukustreaming_v1503.apk](https://github.com/ekajogja/radiobuku/tree/master/build)
+
+2. [v15.04 "Bintang Mataram" --> radiobukustreaming_v1504.apk](https://github.com/ekajogja/radiobuku/tree/master/build)
 
 ___
 
---
+++ app-source/www/js/index_user_scripts.js
@@ -0,0 +1,48 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  .uib_w_2 */
+    $(document).on("click", ".uib_w_2", function(evt)
+    {
+        /* your code goes here */ 
+        intel.xdk.player.startShoutcast("http://103.28.148.18:8254/",true);
+    });
+    
+        /* button  .uib_w_5 */
+    
+    
+        /* button  .uib_w_5 */
+    $(document).on("click", ".uib_w_5", function(evt)
+    {
+         activate_subpage("#uib_page_3"); 
+    });
+    
+        /* button  .uib_w_8 */
+    $(document).on("click", ".uib_w_8", function(evt)
+    {
+         activate_subpage("#uib_page_2"); 
+    });
+    
+        /* button  .uib_w_7 */
+    $(document).on("click", ".uib_w_7", function(evt)
+    {
+         activate_subpage("#uib_page_4"); 
+    });
+    
+        /* button  .uib_w_6 */
+    $(document).on("click", ".uib_w_6", function(evt)
+    {
+        /* your code goes here */
+        intel.xdk.device.showRemoteSite("http://radiobuku.com/kronik-buku/",0,0,50,50);
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
